acfa9aa3080079108fb5b0c26f0e0a1e8b8afafa
Benoit Blanchon <git@benoitblanchon.fr>
2022-04-26 15:18:01 +0200

    Add navigation guards to protect step 3 and 4

--- src/router.js
+++ src/router.js
@@ -3,8 +3,9 @@ import AssistantStep1 from "./pages/Step1Page.vue";
 import AssistantStep2 from "./pages/Step2Page.vue";
 import AssistantStep3 from "./pages/Step3Page.vue";
 import AssistantStep4 from "./pages/Step4Page.vue";
+import { useStore } from "./store";
 
-export default createRouter({
+const router = createRouter({
   history: createWebHashHistory(),
   routes: [
     {
@@ -21,11 +22,17 @@ export default createRouter({
       path: "/step3",
       name: "step3",
       component: AssistantStep3,
+      meta: {
+        requiresNoError: true,
+      },
     },
     {
       path: "/step4",
       name: "step4",
       component: AssistantStep4,
+      meta: {
+        requiresNoError: true,
+      },
     },
     {
       path: "/:pathMatch(.*)*",
@@ -35,3 +42,12 @@ export default createRouter({
   linkActiveClass: "active",
   linkExactActiveClass: "active-exact",
 });
+
+router.beforeEach((to) => {
+  const store = useStore();
+  if (to.meta.requiresNoError && store.hasErrors) {
+    return { name: "step2" };
+  }
+});
+
+export default router;
