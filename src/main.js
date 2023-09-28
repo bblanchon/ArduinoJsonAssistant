583839cb93078e93257872251786e794a778095b
Benoit Blanchon <git@benoitblanchon.fr>
2023-09-28 14:27:00 +0200

    Remove the unused "popover" plugin

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import popover from "./plugins/popover";
 import tooltip from "./plugins/tooltip";
 import router from "./router";
 import { createPinia } from "pinia";
@@ -14,7 +13,6 @@ const el = document.getElementById("assistant-app");
 createApp(App)
   .use(router)
   .use(createPinia())
-  .use(popover)
   .use(tooltip)
   .provide("baseUrl", el.dataset.url || "")
   .provide("version", el.dataset.version)
