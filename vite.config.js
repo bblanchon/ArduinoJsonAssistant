5d20b06fbf89ae88ea454b9cb437c0ae06c82853
Benoit Blanchon <git@benoitblanchon.fr>
2023-09-28 14:41:15 +0200

    Install `bootstrap-icons` and `vite-svg-loader`

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import path from "node:path";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueComponents from 'unplugin-vue-components/vite'
+import vueComponents from "unplugin-vue-components/vite";
+import svgLoader from "vite-svg-loader";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueComponents()],
+  plugins: [vue(), vueComponents(), svgLoader()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
