df3df3046cb2daeede1acc9b47c613bdddb83a2f
Benoit Blanchon <git@benoitblanchon.fr>
2023-09-28 14:21:56 +0200

    Reorganize `main.js`

--- src/main.js
+++ src/main.js
@@ -5,21 +5,19 @@ import router from "./router";
 import { createPinia } from "pinia";
 import { persistStore } from "./persistence";
 
-const el = document.getElementById("assistant-app");
-const sponsors = document.getElementById("assistant-sponsors");
+const sponsorsEl = document.getElementById("assistant-sponsors");
+const sponsors = sponsorsEl ? JSON.parse(sponsorsEl.textContent) : [];
 
-const app = createApp(App);
-app.use(router);
-app.use(createPinia());
+const el = document.getElementById("assistant-app");
+
+createApp(App)
+  .use(router)
+  .use(createPinia())
+  .use(popover)
+  .provide("baseUrl", el.dataset.url || "")
+  .provide("version", el.dataset.version)
+  .provide("scriptUrl", document.currentScript?.src)
+  .provide("sponsors", sponsors)
+  .mount(el);
+
 persistStore();
-app.provide("baseUrl", el.dataset.url || "");
-app.provide("version", el.dataset.version);
-app.provide("scriptUrl", document.currentScript?.src);
-if (sponsors) {
-  sponsors.remove();
-  app.provide("sponsors", JSON.parse(sponsors.textContent));
-} else {
-  app.provide("sponsors", []);
-}
-app.use(popover);
-app.mount(el);
