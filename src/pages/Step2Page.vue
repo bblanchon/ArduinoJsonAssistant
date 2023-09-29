36de11b906fcff96e86041a064760494e55fd8be
Benoit Blanchon <git@benoitblanchon.fr>
2023-09-29 14:53:02 +0200

    Extract the "alerts" store

--- src/pages/Step2Page.vue
+++ src/pages/Step2Page.vue
@@ -98,9 +98,8 @@
 
 <script>
 import { mapActions, mapState, mapWritableState } from "pinia";
-import { hasJsonInJsonSyndrome, measureNesting } from "@/assistant/analyzer";
-import { needsDouble, needsLongLong } from "@/assistant/analyzer";
 import { useConfigStore } from "@/stores/config";
+import { useAlertsStore } from "@/stores/alerts";
 import { measureSize } from "@/assistant/analyzer";
 
 export default {
@@ -114,12 +113,12 @@ export default {
       "filteredInput",
       "filterJson",
       "hasErrors",
-      "hasJsonInJsonSyndrome",
       "input",
       "inputJson",
       "isDeserializing",
       "isSerializing",
     ]),
+    ...mapState(useAlertsStore, ["alerts"]),
     ...mapWritableState(useConfigStore, ["filterEnabled"]),
     filteredInputJson() {
       return JSON.stringify(this.filteredInput, null, 2);
@@ -134,64 +133,6 @@ export default {
       if (this.capacity.recommended > this.cpuInfo.ramError) return "danger";
       if (this.capacity.recommended > this.cpuInfo.ramWarning) return "warning";
       return "success";
-    },
-    alerts() {
-      const longLongNeeded = needsLongLong(this.filteredInput);
-      const longLongIsDefault = !!this.cpuInfo.useLongLong?.default;
-      const doubleNeeded = needsDouble(this.filteredInput);
-      const doubleSupported = !!this.cpuInfo.useDouble;
-      const doubleIsDefault = !!this.cpuInfo.useDouble?.default;
-      const nestingLevel = measureNesting(this.input);
-      return [
-        {
-          if: this.isDeserializing && nestingLevel > this.cpuInfo.nestingLimit,
-          id: "too-deep",
-          type: "warning",
-          message: `This document is deeply nested; don't forget to pass <a href="${this.baseUrl}/v7/api/json/deserializejson/#nesting-limit"><code>DeserializationOption::NestingLimit(${nestingLevel})</code></a>`,
-        },
-        {
-          if: longLongNeeded && !longLongIsDefault,
-          id: "long-long",
-          type: "warning",
-          message: `This document contains <code>long&nbsp;long</code>; you should define <a href="${this.baseUrl}/v7/api/config/use_long_long/"><code>ARDUINOJSON_USE_LONG_LONG</code></a> to <code>1</code>`,
-        },
-        {
-          if: doubleNeeded && doubleSupported && !doubleIsDefault,
-          id: "double",
-          type: "warning",
-          message: `This document contains <code>double</code>; you should define <a href="${this.baseUrl}/v7/api/config/use_double/"><code>ARDUINOJSON_USE_DOUBLE</code></a> to <code>1</code>`,
-        },
-        {
-          if: doubleNeeded && !doubleSupported,
-          id: "double-not-supported",
-          type: "warning",
-          message: `This document contains double-precision floating points values but ${this.cpuInfo.label} doesn't support them (<code>double</code> is the same as <code>float</code>)`,
-        },
-        {
-          if: hasJsonInJsonSyndrome(this.filteredInput),
-          id: "json-in-json",
-          type: "warning",
-          message: `This document suffers from the <q>JSON in JSON</q> syndrome, so you may need to call <a href="${this.baseUrl}/v7/api/json/deserializejson/"><code>deserializeJson()</code></a> multiple times. <strong>The ArduinoJson Assistant doesn't support this scenario.</strong>`,
-        },
-        {
-          if: this.ramColor == "danger",
-          id: "size-error",
-          type: "danger",
-          message: `This is too big to fit in the RAM. See <a href="${this.baseUrl}/v7/how-to/deserialize-a-very-large-document/">How to deserialize a very large document?</a>`,
-        },
-        {
-          if: this.ramColor == "warning",
-          id: "size-warning",
-          type: "warning",
-          message: `This may not fit in the RAM. Make sure there is enough free space.`,
-        },
-        {
-          if: this.ramColor == "danger" && this.cpu === "esp32",
-          id: "esp32-psram",
-          type: "tip",
-          message: `This is too big to fit in the RAM. See <a href="${this.baseUrl}/v7/how-to/use-external-ram-on-esp32/">How to use external RAM on an ESP32?</a>`,
-        },
-      ].filter((alert) => alert.if);
     },
   },
   methods: mapActions(useConfigStore, [
