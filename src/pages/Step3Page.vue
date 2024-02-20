3b5ab18b3c094ca10b8590cc75beff6e083e0530
Benoit Blanchon <git@benoitblanchon.fr>
2024-02-20 18:22:03 +0100

    Converted step 3 to `<script setup>`

--- src/pages/Step3Page.vue
+++ src/pages/Step3Page.vue
@@ -3,26 +3,32 @@
     <h2 class="h4 card-header bg-primary text-white">Step 3: Program</h2>
 
     <div class="card-body">
-      <div class="alert alert-warning" v-if="longLongIsDefault != useLongLong">
+      <div
+        class="alert alert-warning"
+        v-if="board.longLongIsDefault != settings.useLongLong"
+      >
         ⚠️ This program assumes you defined
         <a :href="`${baseUrl}/v7/api/config/use_long_long/`"
           ><code>ARDUINOJSON_USE_LONG_LONG</code></a
         >
-        to <code>{{ +useLongLong }}</code>
+        to <code>{{ +settings.useLongLong }}</code>
       </div>
-      <div class="alert alert-warning" v-if="doubleIsDefault != useDouble">
+      <div
+        class="alert alert-warning"
+        v-if="board.doubleIsDefault != settings.useDouble"
+      >
         ⚠️ This program assumes you defined
         <a :href="`${baseUrl}/v7/api/config/use_double/`"
           ><code>ARDUINOJSON_USE_DOUBLE</code></a
         >
-        to <code>{{ +useDouble }}</code>
+        to <code>{{ +settings.useDouble }}</code>
       </div>
 
-      <div class="form-inline" v-if="isDeserializing">
+      <div class="form-inline" v-if="settings.isDeserializing">
         <label for="io-library" class="sr-only">Name</label>
         <select
           id="io-library"
-          v-model="ioLibrary"
+          v-model="program.ioLibrary"
           class="form-control mb-2 mr-sm-2"
         >
           <option value="serial">Serial</option>
@@ -34,7 +40,7 @@
             class="custom-control-input"
             type="checkbox"
             id="progmem"
-            v-model="progmem"
+            v-model="program.progmem"
           />
           <label class="custom-control-label" for="progmem"
             >Flash strings</label
@@ -56,7 +62,7 @@
           {{ programCopied ? "✓ Copied" : "Copy" }}
         </button>
         <div class="highlight p-3 program">
-          <pre><code class="hljs" v-html="programHtml"></code></pre>
+          <pre><code class="hljs" v-html="program.programHtml"></code></pre>
         </div>
       </figure>
       <ul class="list-inline card-text">
@@ -75,82 +81,59 @@
   </div>
 </template>
 
-<script>
-import { mapState, mapActions, mapWritableState } from "pinia";
+<script setup>
 import "@/assets/highlight.scss";
 
 import { useSettingsStore } from "@/stores/settings";
 import { useBoardStore } from "@/stores/board";
 import { useProgramStore } from "@/stores/program";
 import { sleep } from "@/utils";
+import { inject, computed, ref, watchEffect } from "vue";
 
-export default {
-  inject: ["baseUrl"],
-  data() {
-    return {
-      programCopied: false,
-    };
-  },
-  computed: {
-    ...mapState(useProgramStore, ["programHtml", "programText"]),
-    ...mapWritableState(useProgramStore, ["ioLibrary", "progmem"]),
-    ...mapState(useSettingsStore, [
-      "isDeserializing",
-      "isSerializing",
-      "useLongLong",
-      "useDouble",
-    ]),
-    ...mapState(useBoardStore, ["longLongIsDefault", "doubleIsDefault"]),
-    links() {
-      return [
-        {
-          if: this.isDeserializing,
-          label: "Deserialization Tutorial",
-          url: `${this.baseUrl}/v7/tutorial/deserialization/`,
-        },
-        {
-          if: this.isSerializing,
-          label: "Serialization Tutorial",
-          url: `${this.baseUrl}/v7/tutorial/serialization/`,
-        },
-        {
-          label: "<code>JsonDocument</code>",
-          url: `${this.baseUrl}/v7/api/jsondocument/`,
-        },
-        {
-          if: this.isDeserializing,
-          label: "<code>deserializeJson()</code>",
-          url: `${this.baseUrl}/v7/api/json/deserializejson/`,
-        },
-        {
-          if: this.isSerializing,
-          label: "<code>serializeJson()</code>",
-          url: `${this.baseUrl}/v7/api/json/serializejson/`,
-        },
-      ].filter((link) => link.if);
+const settings = useSettingsStore();
+const board = useBoardStore();
+const program = useProgramStore();
+const baseUrl = inject("baseUrl");
+
+const programCopied = ref(false);
+
+watchEffect(() => program.generate());
+
+const links = computed(() =>
+  [
+    {
+      if: settings.isDeserializing,
+      label: "Deserialization Tutorial",
+      url: `${baseUrl}/v7/tutorial/deserialization/`,
     },
-  },
-  created() {
-    this.generate();
-  },
-  methods: {
-    async copyProgram() {
-      await navigator.clipboard.writeText(this.programText);
-      this.programCopied = true;
-      await sleep(500);
-      this.programCopied = false;
+    {
+      if: settings.isSerializing,
+      label: "Serialization Tutorial",
+      url: `${baseUrl}/v7/tutorial/serialization/`,
     },
-    ...mapActions(useProgramStore, ["generate"]),
-  },
-  watch: {
-    ioLibrary() {
-      this.generate();
+    {
+      label: "<code>JsonDocument</code>",
+      url: `${baseUrl}/v7/api/jsondocument/`,
     },
-    progmem() {
-      this.generate();
+    {
+      if: settings.isDeserializing,
+      label: "<code>deserializeJson()</code>",
+      url: `${baseUrl}/v7/api/json/deserializejson/`,
     },
-  },
-};
+    {
+      if: settings.isSerializing,
+      label: "<code>serializeJson()</code>",
+      url: `${baseUrl}/v7/api/json/serializejson/`,
+    },
+  ].filter((link) => link.if),
+);
+
+async function copyProgram() {
+  await navigator.clipboard.writeText(program.programText);
+  programCopied.value = true;
+  await sleep(500);
+  programCopied.value = false;
+}
 </script>
 
 <style scoped>
