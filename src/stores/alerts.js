7cac65ab2bf0641a99e7e8bf1fd738727335878b
Benoit Blanchon <git@benoitblanchon.fr>
2024-02-21 14:31:28 +0100

    Use a generator function in the "alerts" store

--- src/stores/alerts.js
+++ src/stores/alerts.js
@@ -1,8 +1,8 @@
 import { defineStore } from "pinia";
-import { useSettingsStore } from "./settings";
-import { useBoardStore } from "./board";
 import { computed, inject } from "vue";
 
+import { useSettingsStore } from "./settings";
+import { useBoardStore } from "./board";
 import { useStatsStore } from "./stats";
 
 export const useAlertsStore = defineStore("alerts", () => {
@@ -11,67 +11,77 @@ export const useAlertsStore = defineStore("alerts", () => {
   const settings = useSettingsStore();
   const baseUrl = inject("baseUrl");
 
-  const alerts = computed(() => {
-    return [
-      {
-        if: stats.slotCount > settings.maxSlotCount,
+  function* enumerateAlerts() {
+    if (stats.slotCount > settings.maxSlotCount)
+      yield {
         id: "too-many-slots",
         type: "danger",
         message:
           "<b>This document contains too many values.</b><br>Because of an optimization, <code>JsonDocument</code> can only contain a limited number of values. You could overpass this limit by changing <code>ARDUINOJSON_SLOT_ID_SIZE</code>, but it would increase the memory usage.",
-      },
-      {
-        if:
-          settings.isDeserializing &&
-          stats.nestingLevel > board.memoryModel.nestingLimit,
+      };
+
+    if (
+      settings.isDeserializing &&
+      stats.nestingLevel > board.memoryModel.nestingLimit
+    )
+      yield {
         id: "too-deep",
         type: "warning",
         message: `This document is deeply nested; don't forget to pass <a href="${baseUrl}/v7/api/json/deserializejson/#nesting-limit"><code>DeserializationOption::NestingLimit(${stats.nestingLevel})</code></a>`,
-      },
-      {
-        if: stats.longLongNeeded && !board.longLongIsDefault,
+      };
+
+    if (stats.longLongNeeded && !board.longLongIsDefault)
+      yield {
         id: "long-long",
         type: "warning",
         message: `This document contains <code>long&nbsp;long</code>; you should define <a href="${baseUrl}/v7/config/use_long_long/"><code>ARDUINOJSON_USE_LONG_LONG</code></a> to <code>1</code>`,
-      },
-      {
-        if:
-          stats.doubleNeeded && board.doubleSupported && !board.doubleIsDefault,
+      };
+
+    if (stats.doubleNeeded && board.doubleSupported && !board.doubleIsDefault)
+      yield {
         id: "double",
         type: "warning",
         message: `This document contains <code>double</code>; you should define <a href="${baseUrl}/v7/config/use_double/"><code>ARDUINOJSON_USE_DOUBLE</code></a> to <code>1</code>`,
-      },
-      {
-        if: stats.doubleNeeded && !board.doubleSupported,
+      };
+
+    if (stats.doubleNeeded && !board.doubleSupported)
+      yield {
         id: "double-not-supported",
         type: "warning",
         message: `This document contains double-precision floating points values but ${board.name} doesn't support them (<code>double</code> is the same as <code>float</code>)`,
-      },
-      {
-        if: stats.jsonInJson,
+      };
+
+    if (stats.jsonInJson)
+      yield {
         id: "json-in-json",
         type: "warning",
         message: `This document suffers from the <q>JSON in JSON</q> syndrome, so you may need to call <a href="${baseUrl}/v7/api/json/deserializejson/"><code>deserializeJson()</code></a> multiple times. <strong>The ArduinoJson Assistant doesn't support settings scenario.</strong>`,
-      },
-      {
-        if: stats.ramStatus === "warning",
+      };
+
+    if (stats.ramStatus === "warning")
+      yield {
         id: "size-warning",
         type: "warning",
         message: `This may not fit in the RAM. Make sure there is enough free space.`,
-      },
-      {
-        if: stats.ramStatus === "danger" && !board.psram,
+      };
+
+    if (stats.ramStatus === "danger" && !board.psram)
+      yield {
         id: "size-error",
         type: "danger",
         message: `This is too big to fit in the RAM. See <a href="${baseUrl}/v7/how-to/deserialize-a-very-large-document/">How to deserialize a very large document?</a>`,
-      },
-      {
-        if: stats.ramStatus === "danger" && board.psram,
+      };
+
+    if (stats.ramStatus === "danger" && board.psram)
+      yield {
         id: "esp32-psram",
         type: "tip",
         message: `This is too big to fit in the RAM. See <a href="${baseUrl}/v7/how-to/use-external-ram-on-esp32/">How to use external RAM on an ESP32?</a>`,
-      },
-    ].filter((alert) => alert.if);
+      };
+  }
+
+  const alerts = computed(() => {
+    return Array.from(enumerateAlerts());
   });
 
   return { alerts };
