b12b405c46103b786943c539a4a44f9135ca0933
Benoit Blanchon <git@benoitblanchon.fr>
2025-03-07 16:07:55 +0100

    Update vite URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), svgLoader()],
   resolve: {
