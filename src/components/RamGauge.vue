386f20b223f6019c7a23fb6a69678f5ba17b7cf9
Benoit Blanchon <git@benoitblanchon.fr>
2025-03-07 11:28:18 +0100

    More components

--- src/components/RamGauge.vue
+++ src/components/RamGauge.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="d-flex flex-column align-items-center">
     <h3 class="h5">RAM usage</h3>
-    <BaseGauge
-      :value="stats.peakRamUsage"
-      :max="board.ram"
-      :warning="0.5"
-      :danger="0.75"
-      :value-text="formatBytes(stats.finalDocSize)"
-      title="RAM usage"
-      :label="formatBytes(stats.peakRamUsage) + ' peak'"
-      bytes
-    />
+    <BaseGauge :value="stats.peakRamUsage" :max="board.ram" :warning="0.5" :danger="0.75"
+      :value-text="formatBytes(stats.finalDocSize)" title="RAM usage" :label="formatBytes(stats.peakRamUsage) + ' peak'"
+      bytes />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import bytes from "bytes";
 
 import { useStatsStore } from "@/stores/stats";
@@ -25,7 +18,7 @@ import BaseGauge from "@/components/BaseGauge.vue";
 const stats = useStatsStore();
 const board = useBoardStore();
 
-function formatBytes(values) {
-  return bytes.format(values, { decimalPlaces: 1, unitSeparator: " " });
+function formatBytes(values: number) {
+  return bytes.format(values, { decimalPlaces: 1, unitSeparator: " " }) as string;
 }
 </script>
--- src/components/StringsGauge.vue
+++ src/components/StringsGauge.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="d-flex flex-column align-items-center">
     <h3 class="h5">String length</h3>
-    <BaseGauge
-      :value="stats.maxStringLength"
-      :max="settings.maxStringLength"
-      :warning="0.8"
-      :danger="0.95"
-    />
+    <BaseGauge :value="stats.maxStringLength" :max="settings.maxStringLength" :warning="0.8" :danger="0.95" />
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { useStatsStore } from "@/stores/stats";
 import { useSettingsStore } from "@/stores/settings";
 
