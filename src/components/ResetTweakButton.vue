e8c1581039e9201503e5b62f034565cf5b66f7fa
Benoit Blanchon <git@benoitblanchon.fr>
2025-03-07 16:26:16 +0100

    ResetTweakButton: upgrade to `<script setup>`

--- src/components/ResetTweakButton.vue
+++ src/components/ResetTweakButton.vue
@@ -1,12 +1,21 @@
 <template>
-  <button v-show="modelValue != defaultValue" type="button" class="btn btn-outline-warning btn-sm py-0 mx-1"
-    style="margin-top: -3px" @click="$emit('update:modelValue', defaultValue)">
+  <button
+    v-show="value != defaultValue"
+    type="button"
+    class="btn btn-outline-warning btn-sm py-0 mx-1"
+    style="margin-top: -3px"
+    @click="value = defaultValue"
+  >
     Reset
   </button>
 </template>
 
-<script lang="ts">
-export default {
-  props: ["modelValue", "defaultValue"],
-};
+<script setup lang="ts">
+type ValueType = boolean | number;
+
+const value = defineModel<ValueType>();
+
+defineProps<{
+  defaultValue: ValueType;
+}>();
 </script>
