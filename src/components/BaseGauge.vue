ef27075045aeb7af02477db0d05be73974c837bd
Benoit Blanchon <git@benoitblanchon.fr>
2025-04-04 09:52:42 +0200

    BaseGauge: modernize

--- src/components/BaseGauge.vue
+++ src/components/BaseGauge.vue
@@ -3,48 +3,22 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted, watch } from "vue";
-import JustGage from "justgage";
-
-const props = defineProps({
-  value: {
-    type: Number,
-    required: true,
-  },
-  valueText: {
-    type: String,
-    default: "",
-  },
-  warning: {
-    type: Number,
-    default: 0.5,
-  },
-  danger: {
-    type: Number,
-    default: 0.75,
-  },
-  max: {
-    type: Number,
-    default: 100,
-  },
-  label: {
-    type: String,
-    default: "",
-  },
-  bytes: {
-    type: Boolean,
-    default: false,
-  },
-});
-
-const el = ref();
-const gauge = ref<JustGage | null>(null);
-
-const green = "#28a745";
-const yellow = "#ffc107";
-const red = "#dc3545";
-
 import { format } from "bytes";
+import JustGage from "justgage";
+import { onMounted, ref, useTemplateRef, watch } from "vue";
+
+const props = defineProps<{
+  value: number;
+  valueText?: string;
+  warning?: number;
+  danger?: number;
+  max?: number;
+  label?: string;
+  bytes?: boolean;
+}>();
+
+const el = useTemplateRef("el");
+const gauge = ref<JustGage>();
 
 function formatValue(value: number) {
   const result = format(value, { decimalPlaces: 1 });
@@ -56,29 +30,30 @@ function createGauge() {
     parentNode: el.value,
     max: props.max,
     value: props.value,
-    label: props.label,
+    label: props.label ?? "",
     textRenderer: props.valueText ? () => props.valueText : formatValue,
-    maxTxt: formatValue(props.max),
-    customSectors: {
-      percents: true, // lo and hi values are in %
-      ranges: [
-        {
-          color: green,
-          lo: 0,
-          hi: props.warning * 100,
-        },
-        {
-          color: yellow,
-          lo: props.warning * 100,
-          hi: props.danger * 100,
-        },
-        {
-          color: red,
-          lo: props.danger * 100,
-          hi: 100,
-        },
-      ],
-    },
+    maxTxt: props.max && formatValue(props.max),
+    customSectors: props.warning &&
+      props.danger && {
+        percents: true, // lo and hi values are in %
+        ranges: [
+          {
+            color: "#28a745",
+            lo: 0,
+            hi: props.warning * 100,
+          },
+          {
+            color: "#ffc107",
+            lo: props.warning * 100,
+            hi: props.danger * 100,
+          },
+          {
+            color: "#dc3545",
+            lo: props.danger * 100,
+            hi: 100,
+          },
+        ],
+      },
   });
 }
 
